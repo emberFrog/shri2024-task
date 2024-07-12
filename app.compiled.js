d140c84e1db21d7d914307d9cccb18fc5ec5b42b
Nikita Polianskii <157965326+emberFrog@users.noreply.github.com>
2024-07-12 12:33:47 +0300

    Minor fixes

--- app.compiled.js
+++ app.compiled.js
@@ -347,7 +347,5 @@ function Main() {
     onClick: onArrowCLick
   }))));
 }
-setTimeout(() => {
-  const root = ReactDOM.createRoot(document.getElementById('app'));
-  root.render( /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Header, null), /*#__PURE__*/React.createElement(Main, null)));
-}, 100);
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render( /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Header, null), /*#__PURE__*/React.createElement(Main, null)));
